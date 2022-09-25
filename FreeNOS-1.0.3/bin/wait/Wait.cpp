fba905cd486569dd58f230898f9d9ebb755ed6fc
Davist2063 <davistong2981@gmail.com>
2022-09-24 22:21:21 -0700

    Added Complete Message and Fixed Error Messages

--- FreeNOS-1.0.3/bin/wait/Wait.cpp
+++ FreeNOS-1.0.3/bin/wait/Wait.cpp
@@ -12,7 +12,7 @@
 Wait::Wait(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
-    parser().setDescription("Wait for process to finishh");
+    parser().setDescription("Wait for process to finish");
     parser().registerPositional("PROCESS", "Pause until given process is done");
 }
 
@@ -28,10 +28,23 @@ Wait::Result Wait::exec()
     ProcessClient::Info info;
 
         const ProcessClient::Result result = process.processInfo(PID, info); //process info gets info by id -- other methods do so by name
-        if (result == ProcessClient::Success)
+        if(PID < 0)
         {
+            ERROR("Invalid process ID");
+            return InvalidArgument;
+        }
+        else if(PID > ProcessClient::MaximumProcesses)
+        {
+            ERROR("Maximum number of processes is 1024");
+            return InvalidArgument;
+        }
+        else if (result == ProcessClient::Success)
+        {
+            if(PID < 17)
+            {
+                ERROR("Potentially dangerous execution");
+            }
             DEBUG("PID " << PID << " state = " << *info.textState);
-
             char line[128];
             snprintf(line, sizeof(line), //this is the outputted table after you 'ps' and its formatted content
                     "%7d %4d %5d %10s %32s\r\n",
@@ -40,23 +53,13 @@ Wait::Result Wait::exec()
             out << line;
             write(1, *out, out.length());
             waitpid(PID, &status, 0);
+            out = "";
+            out << "Process [" << PID << "]+      Done : " << *info.command << "\n";
+            write(1, *out, out.length());
         }
-
-        if(PID > ProcessClient::MaximumProcesses)
-        {
-            ERROR("Maximum number of processes is 1024");
+        else if(result == ProcessClient::NotFound || result == ProcessClient::IOError) {
+            ERROR("Not an existing process");
             return InvalidArgument;
         }
-        else if(PID < 0)
-        {
-            ERROR("Invalid process ID");
-            return InvalidArgument;
-        }
-        else if(PID < 17)
-        {
-            ERROR("Potentially dangerous execution");
-            return InvalidArgument;
-        }
-
         return Success;
 }
